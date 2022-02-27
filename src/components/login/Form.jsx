68cbba9027d7f8d3ca8230543fa2af7e5cb0b838
Pratham Yadav <pyadav5000@gmail.com>
2022-02-27 14:09:06 +0530

    changed layout of register page and added show and hide password functionality

--- src/components/login/Form.jsx
+++ src/components/login/Form.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Link } from 'react-router-dom'
 
 
 export default function Form() {
+
+    const password = useRef(null);
+    const hideShowImage = useRef(null);
+
+
+    const changePasswordType = (e) => {
+
+
+        if (password.current.type === 'password') {
+            password.current.type = 'text';
+            hideShowImage.current.src = '/centaur-web/images/icons/eye_open.svg';
+        } else {
+            password.current.type = 'password';
+            hideShowImage.current.src = '/centaur-web/images/icons/eye_close.svg';
+        }
+    }
+
     return (
         <form className='login_page_form'>
 
-            <div className='page_form_thirdParty'>
-                <button className='form_thirdParty_section'>
-                    <img src='/centaur-web/images/icons/google.svg' alt="" width={30} />
-                    <p>Login With Google</p>
-                </button>
 
-            </div>
 
             <div className='page_form_div'>
                 <label>Email</label>
@@ -20,7 +31,18 @@ export default function Form() {
             </div>
             <div className='page_form_div'>
                 <label>Password</label>
-                <input type="password" placeholder='********' />
+                <div className='form_div_password_input'>
+                    <input
+                        type="password" placeholder='********'
+                        ref={password}
+                    />
+                    <button onClick={changePasswordType} type='button'>
+                        <img src="/centaur-web/images/icons/eye_open.svg" alt=''
+
+                            ref={hideShowImage}
+                        />
+                    </button>
+                </div>
             </div>
 
             <div className='page_form_links'>
@@ -29,6 +51,16 @@ export default function Form() {
             </div>
 
             <Link to="/dashboard" className='page_form_btn'>Login</Link>
+
+            <div className='page_form_thirdParty'>
+                <button
+                    type='button'
+                    className='form_thirdParty_section'>
+                    <img src='/centaur-web/images/icons/google.svg' alt="" width={30} />
+                    <p>Login With Google</p>
+                </button>
+
+            </div>
         </form>
     )
 }
--- src/components/register/form.jsx
+++ src/components/register/form.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react'
-import { Link, Navigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import Popup from '../../layout/lyt-popup';
 
 export default function RegForm() {
@@ -8,6 +8,8 @@ export default function RegForm() {
 
     const [userType, setUserType] = React.useState('');
     const [isValidKey, setIsValidKey] = React.useState(false);
+
+    const Navigate = useNavigate();
 
     const [isOpen, setIsOpen] = React.useState(false);
 
@@ -18,15 +20,44 @@ export default function RegForm() {
     const confirmPassword = useRef(null);
     const enrollmentNumber = useRef(null);
     const phoneNumber = useRef(null);
-
+    const hideShowImage = useRef(null);
+    const hideShowImage2 = useRef(null);
 
     const validateKey = async () => {
         const userEnteredKey = await license.current.value;
+        console.log(license.current.disabled);
         if (userEnteredKey === serialNumber) {
             setIsValidKey(true);
             license.current.className += " validInput";
+            license.current.disabled = true;
+        } else {
+            setIsValidKey(false);
+            license.current.className += " invalidInput";
+            license.current.disabled = false;
         }
 
+    }
+
+    const changePasswordType = (e) => {
+
+        if (password.current.type === 'password') {
+            password.current.type = 'text';
+            hideShowImage.current.src = '/centaur-web/images/icons/eye_open.svg';
+        } else {
+            password.current.type = 'password';
+            hideShowImage.current.src = '/centaur-web/images/icons/eye_close.svg';
+        }
+    }
+
+    const changeConfirmPasswordType = (e) => {
+        console.log(hideShowImage2.current.src);
+        if (confirmPassword.current.type === 'password') {
+            confirmPassword.current.type = 'text';
+            hideShowImage2.current.src = '/centaur-web/images/icons/eye_open.svg';
+        } else {
+            confirmPassword.current.type = 'password';
+            hideShowImage2.current.src = '/centaur-web/images/icons/eye_close.svg';
+        }
     }
 
 
@@ -36,7 +67,9 @@ export default function RegForm() {
         e.preventDefault();
         if (name.current.value === '' || email.current.value === '' || password.current.value === '' || confirmPassword.current.value === '' || enrollmentNumber.current.value === '' || phoneNumber.current.value === '') {
             setIsOpen(true);
+            alert('Please fill all the fields');
         } else {
+            console.log(name.current.value);
             Navigate('/login');
         }
 
@@ -47,19 +80,9 @@ export default function RegForm() {
 
 
             {/* Still working on this popup box */}
-            {isOpen && <Popup onClose={() => setIsOpen(false)}>
+            {/* {isOpen && <Popup onClose={() => setIsOpen(false)}>
                 <p className='popupAlert'>Please fill all the details</p>
-            </Popup>}
-
-
-            <div className='page_form_thirdParty'>
-                <button className='form_thirdParty_section'>
-                    <img src='/centaur-web/images/icons/google.svg' alt="" width={30} />
-                    <p>Login With Google</p>
-                </button>
-
-            </div>
-
+            </Popup>} */}
 
             <div className='page_form_div'>
                 <label>User</label>
@@ -69,75 +92,102 @@ export default function RegForm() {
                     <option value="staff">Staff</option>
                 </select>
             </div>
+            <div className='page_form_wrapper'>
 
-            {
-                userType === 'staff' &&
+                {
+                    userType === 'staff' &&
+                    <div className='page_form_div'>
+                        <label>License</label>
+                        <input
+                            type="text" placeholder='XXXXXX-XXXXXX-XXXXXX-XXXXXX-XXXXXX-XXXXXX'
+                            ref={license}
+                            onChange={validateKey}
+                        />
+                    </div>
+                }
+
                 <div className='page_form_div'>
-                    <label>License</label>
+                    <label>Name</label>
                     <input
-                        type="text" placeholder='XXXXXX-XXXXXX-XXXXXX-XXXXXX-XXXXXX-XXXXXX'
-                        ref={license}
-                        onChange={validateKey}
+                        type="text"
+                        placeholder='Your name'
+                        ref={name}
+                        disabled={!isValidKey && userType === 'staff'}
                     />
                 </div>
-            }
 
-            <div className='page_form_div'>
-                <label>Name</label>
-                <input
-                    type="text"
-                    placeholder='Your name'
-                    ref={name}
-                    disabled={!isValidKey && userType === 'staff'}
-                />
-            </div>
+                <div className='page_form_div'>
+                    <label>Email</label>
+                    <input
+                        type="email"
+                        placeholder='name@example.com'
+                        ref={email}
+                        disabled={!isValidKey && userType === 'staff'}
+                    />
+                </div>
+                <div className='page_form_div'>
+                    <label>Password</label>
+                    <div className='form_div_password_input'>
 
-            <div className='page_form_div'>
-                <label>Email</label>
-                <input
-                    type="email"
-                    placeholder='name@example.com'
-                    ref={email}
-                    disabled={!isValidKey && userType === 'staff'}
-                />
-            </div>
-            <div className='page_form_div'>
-                <label>Password</label>
-                <input
-                    type="password"
-                    placeholder='********'
-                    ref={password}
-                    disabled={!isValidKey && userType === 'staff'}
-                />
-            </div>
+                        <input
+                            type="password"
+                            placeholder='********'
+                            ref={password}
+                            disabled={!isValidKey && userType === 'staff'}
+                        />
+                        <button
+                            onClick={changePasswordType} type='button'
+                        >
+                            <img
+                                src="/centaur-web/images/icons/eye_close.svg"
+                                alt=''
+                                ref={hideShowImage}
+                            />
+                        </button>
+                    </div>
+                </div>
 
-            <div className='page_form_div'>
-                <label>Confirm Password</label>
-                <input
-                    type="password"
-                    placeholder='********'
-                    ref={confirmPassword}
-                    disabled={!isValidKey && userType === 'staff'}
-                />
-            </div>
-            <div className='page_form_div'>
-                <label>Enrollment Number</label>
-                <input
-                    type="number"
-                    placeholder='EN-XXXXXXXX'
-                    ref={enrollmentNumber}
-                    disabled={!isValidKey && userType === 'staff'}
-                />
-            </div>
+                <div className='page_form_div'>
+                    <label>Confirm Password</label>
+                    <div className='form_div_password_input'>
+                        <input
+                            type="password"
+                            placeholder='********'
+                            ref={confirmPassword}
+                            disabled={!isValidKey && userType === 'staff'}
+                        />
+                        <button
+                            type='button'
+                            onClick={changeConfirmPasswordType}
+                        >
+                            <img
+                                src="/centaur-web/images/icons/eye_close.svg"
+                                alt=""
+                                ref={hideShowImage2}
+                            />
+                        </button>
 
-            <div className='page_form_div'>
-                <label>Phone number</label>
-                <input
-                    type="number"
-                    placeholder='+91 XXXXXXXX'
-                    ref={phoneNumber}
-                    disabled={!isValidKey && userType === 'staff'}
-                />
+                    </div>
+                </div>
+                <div className='page_form_div'>
+                    <label>Enrollment Number</label>
+                    <input
+                        type="number"
+                        placeholder='EN-XXXXXXXX'
+                        ref={enrollmentNumber}
+                        disabled={!isValidKey && userType === 'staff'}
+                    />
+                </div>
+
+                <div className='page_form_div'>
+                    <label>Phone number</label>
+                    <input
+                        type="number"
+                        placeholder='+91 XXXXXXXX'
+                        ref={phoneNumber}
+                        disabled={!isValidKey && userType === 'staff'}
+                    />
+                </div>
             </div>
 
             <div className='page_form_links'>
@@ -147,6 +197,16 @@ export default function RegForm() {
             <button to="/login" className='page_form_btn'
                 onClick={handleSubmit}
             >Register</button>
+
+            <div className='page_form_thirdParty'>
+                <button
+                    type='button'
+                    className='form_thirdParty_section'>
+                    <img src='/centaur-web/images/icons/google.svg' alt="" width={30} />
+                    <p>Login With Google</p>
+                </button>
+
+            </div>
         </form>
     )
 }
