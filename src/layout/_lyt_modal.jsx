be36418e51b0cbe159cad4948753113460e94091
Pratham Yadav <pyadav5000@gmail.com>
2022-03-03 23:53:13 +0530

    Added alert popup and modal in myWork

--- src/layout/_lyt_modal.jsx
+++ src/layout/_lyt_modal.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-export default function ModalLayout() {
+
+// just need to setup children
+export default function ModalLayout({ setIsOpen, title, children }) {
+
+    const taskTitle = useRef(null);
+    const taskDescription = useRef(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const title = taskTitle.current.value;
+        const description = taskDescription.current.value;
+        console.log({ title, description });
+    }
+
     return (
-        <div>ModalLayout</div>
+        <div className='bg_modal_wrapper'>
+            <div className='modal_wrapper_center'>
+                <div className='wrapper_center_modal'>
+                    <div className='center_modal_header'>
+                        <h2>Create Task</h2>
+                    </div>
+                    <button
+                        className='center_modal_closeBtn btn'
+                        onClick={() => setIsOpen(false)}
+                    >
+                        <img src="/centaur-web/images/icons/cross-red.svg" alt="" />
+                    </button>
+                    <div className='center_modal_content'>
+                        <form className='modal_content_form'>
+                            <input type="text" ref={taskTitle} placeholder='Task Title' />
+                            <textarea
+                                ref={taskDescription}
+                                className='content_form_noteInput' placeholder='Note...' />
+
+                            <div className='content_form_actionBtn'>
+                                <button className='btn' onClick={handleSubmit}>
+                                    <p>Add Task</p>
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/layout/lyt-popup.jsx
+++ src/layout/lyt-popup.jsx
@@ -1,28 +1,20 @@
 import React, { useEffect } from 'react'
 
-export default function Popup({ children, onClose }) {
-
-    const [isOpen, setIsOpen] = React.useState(true);
-
-    useEffect(() => {
-        const time = setTimeout(() => {
-            setIsOpen(false);
-        }, 3000);
-
-        return () => {
-            onClose();
-            clearTimeout(time)
-        };
-    }, []);
-
-
-    if (!isOpen) return null;
+export default function Popup({ textColor, text, setIsOpen, children }) {
 
     return (
-        <section className={`wrapper_popup_modal animate`}>
-            <div className='popup_modal_container' >
-                {children}
+        <section className='wrapper_popup_bg'>
+            <div className='wrapper_popup_center'>
+                <div className='popup_center_content'>
+                    <div className='center_content_header'>
+                        <h1>{text}</h1>
+                    </div>
+                    <div className='center_content_actions'>
+                        <button onClick={() => setIsOpen(false)} className='btn'>Ok</button>
+                    </div>
+                </div>
             </div>
+
         </section>
     )
 }
