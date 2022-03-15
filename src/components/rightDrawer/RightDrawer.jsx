e7bbbfce87e5c55bfa0f74caf9abd0f8e7f579a7
Pratham Yadav <72993471+ypratham@users.noreply.github.com>
2022-03-15 21:30:15 +0530

    Added stopPropagation to drawers

--- src/components/rightDrawer/RightDrawer.jsx
+++ src/components/rightDrawer/RightDrawer.jsx
@@ -8,10 +8,14 @@ export default function Modal({ show, handleClose, children }) {
 
     return (
         <section className={`modalContainer ${show && 'displayLeft'} `} onClick={(e) => {
-            e.stopPropagation();
             handleClose();
         }}>
-            <div className={`modalMain ${show && 'displayRight'}`}>
+            <div className={`modalMain ${show && 'displayRight'}`}
+                onClick={(e) => {
+
+                    e.stopPropagation();
+                }}
+            >
                 <button className='closeButton' onClick={handleClose}>
                     <AddIcon />
                 </button>
