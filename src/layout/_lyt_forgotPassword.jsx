24bdc3d6f92958d3daa4524a40cc4a9975bb9cc4
Pratham Yadav <72993471+ypratham@users.noreply.github.com>
2022-03-20 18:46:26 +0530

    Forgot password page developed

---
+++ src/layout/_lyt_forgotPassword.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { useNavigate } from 'react-router';
+import NavBar from '../components/landing/navBar';
+
+export default function ForgotPasswordPage() {
+
+    const navigate = useNavigate();
+
+    const [enteredEmail, setEnteredEmail] = React.useState('');
+
+    const handleInputChange = (e) => {
+        setEnteredEmail(e.target.value);
+    }
+
+    const handleButtonClick = (e) => {
+        e.preventDefault();
+        console.log(enteredEmail);
+        navigate('/login');
+    }
+
+
+
+    return (
+        <section className='lyt_wrapper_forgotPassword'>
+            <NavBar />
+
+            <form className='wrapper_forgotPassword_form'>
+                <h2>Forgot Password?</h2>
+                <p>
+                    Don't worry! It happens. Please enter your email address and we'll send you a link to reset your password.
+                </p>
+                <div className='forgotPassword_form_inputGrp'>
+                    <label>Email</label>
+                    <input type="email"
+                        placeholder='example@gmail.com'
+                        value={enteredEmail}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div className='forgotPassword_form_btn'>
+                    <button
+                        className='forgotPassword_form_btn'
+                        onClick={handleButtonClick}
+                    >
+                        Send
+                    </button>
+                </div>
+
+            </form>
+
+        </section>
+    )
+}
