7f13a6ccda905050ebd13616e3daa0ec333e9939
Pratham Yadav <72993471+ypratham@users.noreply.github.com>
2022-03-20 12:45:23 +0530

    members and chats section completed

--- src/components/departments/Members/Members.jsx
+++ src/components/departments/Members/Members.jsx
@@ -2,23 +2,58 @@ import React from 'react'
 
 export default function Members() {
     return (
-        <section>
+        <section className='cp_wrapper_members'>
             <h2>Members</h2>
-            <ul className='wrapper_members_list'>
-                <li className='members_list_item'>
-                    <div className='list_item_avatar'>
-                        <img src="https://via.placeholder.com/150" alt="avatar" />
-                    </div>
+            <div className='wrapper_members_section'>
 
-                    <p className='list_item_name'>
-                        Pratham Yadav
-                    </p>
-                    <p className='list_item_role'>
-                        Student
-                    </p>
+                <div className='members_section_label'>
+                    <p>Faculty</p>
+                </div>
 
-                </li>
-            </ul>
+                <ul className='wrapper_members_list'>
+                    {
+                        [1, 2, 3, 4, 5, 6, 7].map(() => {
+                            return <li className='members_list_item'>
+                                <div className='list_item_avatar'>
+                                    <img src="https://via.placeholder.com/150" alt="avatar" />
+                                </div>
+
+                                <p className='list_item_name'>
+                                    Bedant Hota
+                                </p>
+                                <p className='list_item_role'>
+                                    Faculty
+                                </p>
+
+                            </li>
+                        })
+                    }
+                </ul>
+            </div>
+            <div className='wrapper_members_section'>
+                <div className='members_section_label'>
+                    <p>Student</p>
+                </div>
+                <ul className='wrapper_members_list'>
+                    {
+                        [1, 2, 3, 4, 5, 6, 7].map(() => {
+                            return <li className='members_list_item'>
+                                <div className='list_item_avatar'>
+                                    <img src="https://via.placeholder.com/150" alt="avatar" />
+                                </div>
+
+                                <p className='list_item_name'>
+                                    Pratham Yadav
+                                </p>
+                                <p className='list_item_role'>
+                                    Student
+                                </p>
+
+                            </li>
+                        })
+                    }
+                </ul>
+            </div>
 
         </section>
     )
