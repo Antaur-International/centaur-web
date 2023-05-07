3e967de7f2c4ebd72526554788f7e58c34d0ceb9
ypratham <ypratham0014@gmail.com>
2023-05-07 13:54:48 +0530

    Meet fixed

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,143 +1,164 @@
-import React from 'react';
-import Header from "./Header"
+import React from "react";
+import Header from "./Header";
 import "./dashboard.css";
-import { CalenderIcon, MoreIcon, VideoIcon } from '../icons/Icons';
-import { BoxLoading } from 'react-loadingg';
-import TodoLayout from '../layout/lyt-todo';
-import Modal from './rightDrawer/RightDrawer';
-import Newsstand from './newsstand/newsstand';
-import { API_HOST } from '../API/constant';
-import axios from 'axios';
-import { useAuth } from '../data/Context/UserContext';
-import { EmptyMeet } from './EmptyState/EmptyMeet';
-
+import { CalenderIcon, MoreIcon, VideoIcon } from "../icons/Icons";
+import { BoxLoading } from "react-loadingg";
+import TodoLayout from "../layout/lyt-todo";
+import Modal from "./rightDrawer/RightDrawer";
+import Newsstand from "./newsstand/newsstand";
+import { API_HOST } from "../API/constant";
+import axios from "axios";
+import { useAuth } from "../data/Context/UserContext";
+import { EmptyMeet } from "./EmptyState/EmptyMeet";
 
 const MeetingsListItem = ({ user, meeting }) => {
+  let date = new Date(meeting.meet_date);
 
-    let date = new Date(meeting.meet_date);
+  const JoinMeeting = (meet_id) => {
+    window.open(
+      `https://centaur-meet.onrender.com/${meet_id}?userId=${user._id}`,
+      "_blank"
+    );
+    // window.open(`http://localhost:4000/${meet_id}?userId=${user._id}`, "_blank");
+  };
 
-    const JoinMeeting = (meet_id) => {
-        window.open(`https://grp-call-peer-js.herokuapp.com/${meet_id}?userId=${user._id}`, "_blank");
-        // window.open(`http://localhost:4000/${meet_id}?userId=${user._id}`, "_blank");
-    }
-
-    return <li>
-        <div>
-            <h3 className='meeting_title'>{meeting.meet_title}</h3>
-            <button onClick={() => JoinMeeting(meeting._id)} className='meeting_joinBtn'><VideoIcon /></button>
-        </div>
-        <p className='meeting_desc'>{meeting.meet_description}</p>
-        <p className='meeting-time'>{date.getDate() + "-" + date.getMonth() + "-" + date.getFullYear()}</p>
+  return (
+    <li>
+      <div>
+        <h3 className="meeting_title">{meeting.meet_title}</h3>
+        <button
+          onClick={() => JoinMeeting(meeting._id)}
+          className="meeting_joinBtn"
+        >
+          <VideoIcon />
+        </button>
+      </div>
+      <p className="meeting_desc">{meeting.meet_description}</p>
+      <p className="meeting-time">
+        {date.getDate() + "-" + date.getMonth() + "-" + date.getFullYear()}
+      </p>
     </li>
-}
+  );
+};
 
 const NewsCardItem = (props) => {
-    return <li className='wrapper_newsCard_item'>
-        <div className='newsCard_item_head'>
-            <p className='item_head_category'>{props.category}</p>
-        </div>
+  return (
+    <li className="wrapper_newsCard_item">
+      <div className="newsCard_item_head">
+        <p className="item_head_category">{props.category}</p>
+      </div>
 
-        <p className='newsCard_item_content'>
-            {props.content}
-        </p>
+      <p className="newsCard_item_content">{props.content}</p>
     </li>
-}
+  );
+};
 
 export default function Dashboard({ user, meetings }) {
-    const [greeting, setGreeting] = React.useState("Evening");
-    const [isOpen, setIsOpen] = React.useState(false);
-    const [news, setNews] = React.useState([]);
-    const { userInstance, isAuthenticated } = useAuth();
+  const [greeting, setGreeting] = React.useState("Evening");
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [news, setNews] = React.useState([]);
+  const { userInstance, isAuthenticated } = useAuth();
 
-    const handleOpen = () => {
-        setIsOpen(true);
-    }
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
 
-    const handleClose = () => {
-        setIsOpen(false);
-    }
+  const handleClose = () => {
+    setIsOpen(false);
+  };
 
-    React.useEffect(() => {
-        const date = new Date();
-        const hours = date.getHours();
-        const greetingTemp = hours < 12 ? "Morning" : hours < 18 ? "Afternoon" : "Evening";
-        setGreeting(greetingTemp)
+  React.useEffect(() => {
+    const date = new Date();
+    const hours = date.getHours();
+    const greetingTemp =
+      hours < 12 ? "Morning" : hours < 18 ? "Afternoon" : "Evening";
+    setGreeting(greetingTemp);
 
-        axios
-            .get(`${API_HOST}/msbte/news`)
-            .then(res => {
-                // console.log(res.data.newsData);
-                setNews(res.data.newsData);
-            })
-            .catch(err => {
-                console.log(err);
-            });
+    axios
+      .get(`${API_HOST}/msbte/news`)
+      .then((res) => {
+        // console.log(res.data.newsData);
+        setNews(res.data.newsData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    }, []);
+  if (!isAuthenticated) {
+    return <BoxLoading color="#65FF52" />;
+  }
 
-    if (!isAuthenticated) {
-        return <BoxLoading color="#65FF52" />
-    }
+  return (
+    <section className="dashboard">
+      <Modal show={isOpen} handleClose={handleClose}>
+        <Newsstand />
+      </Modal>
+      <Header />
+      <section className="welcome">
+        <img src="https://i.ibb.co/XpFJZS0/fox-image.png" alt="" />
+        <div>
+          <h2>
+            Good {greeting}, <br />{" "}
+            <b style={{ fontSize: "2vw" }}>{userInstance.name}</b>
+          </h2>
+          <button>Go to Schedules </button>
+        </div>
+      </section>
+      <section className="calender">
+        <div className="dashboardCardLabel">
+          <div className="topLayer">
+            <p>Todo</p>
+            <MoreIcon />
+          </div>
+        </div>
+        <TodoLayout user={userInstance} />
+      </section>
+      <section className="news" onClick={handleOpen}>
+        <div className="dashboardCardLabel newsStand">
+          <div className="topLayer">
+            <p>Newsstand</p>
+            <MoreIcon />
+          </div>
 
-
-    return <section className='dashboard'>
-        <Modal show={isOpen} handleClose={handleClose} >
-            <Newsstand />
-        </Modal>
-        <Header />
-        <section className='welcome'>
-            <img src="https://i.ibb.co/XpFJZS0/fox-image.png" alt="" />
-            <div>
-                <h2>Good {greeting}, <br />  <b style={{ fontSize: "2vw" }}>{userInstance.name}</b></h2>
-                <button>Go to Schedules </button>
-            </div>
-        </section>
-        <section className='calender'>
-            <div className='dashboardCardLabel'>
-                <div className='topLayer'>
-                    <p>Todo</p>
-                    <MoreIcon />
-                </div>
-            </div>
-            <TodoLayout user={userInstance} />
-        </section>
-        <section className='news' onClick={handleOpen}>
-            <div className='dashboardCardLabel newsStand'>
-                <div className='topLayer'>
-                    <p>Newsstand</p>
-                    <MoreIcon />
-                </div>
-
-                <section className='cp_newsstand_wrapper'>
-                    <ul className='wrapper_newsCard_list'>
-                        {
-                            news.map(news => {
-                                return <NewsCardItem
-                                    key={news.id}
-                                    category="MSBTE"
-                                    content={news.innerText}
-                                />
-                            })
-                        }
-                    </ul>
-                </section>
-            </div>
-        </section>
-        <section className='meetings'>
-            <div className='dashboardCardLabel'>
-                <div className='topLayer'>
-                    <p>Upcoming Meetings</p>
-                    <CalenderIcon />
-                </div>
-            </div>
-            <ul className='meetings_list'>
-                {
-                    meetings.map.length > 0 ? meetings.map(meeting => {
-                        return <MeetingsListItem key={meeting._id} user={userInstance} meeting={meeting} />
-                    }) : <EmptyMeet />
-                }
-
+          <section className="cp_newsstand_wrapper">
+            <ul className="wrapper_newsCard_list">
+              {news.map((news) => {
+                return (
+                  <NewsCardItem
+                    key={news.id}
+                    category="MSBTE"
+                    content={news.innerText}
+                  />
+                );
+              })}
             </ul>
-        </section>
-    </section>;
+          </section>
+        </div>
+      </section>
+      <section className="meetings">
+        <div className="dashboardCardLabel">
+          <div className="topLayer">
+            <p>Upcoming Meetings</p>
+            <CalenderIcon />
+          </div>
+        </div>
+        <ul className="meetings_list">
+          {meetings.map.length > 0 ? (
+            meetings.map((meeting) => {
+              return (
+                <MeetingsListItem
+                  key={meeting._id}
+                  user={userInstance}
+                  meeting={meeting}
+                />
+              );
+            })
+          ) : (
+            <EmptyMeet />
+          )}
+        </ul>
+      </section>
+    </section>
+  );
 }
